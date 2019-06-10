397adadcf85ec42cbf5684158d49cdb57e029b8c
Tarini-Tyagi <41193726+Tarini-Tyagi@users.noreply.github.com>
2019-06-10 22:52:12 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+print("Hello " + name)
+rm = 95 - int(age)
+year = 2019+rm
+print("You will be 95 years old in the year " + str(year))
